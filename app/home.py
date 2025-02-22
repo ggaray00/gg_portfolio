eb012079845bb31afc8315305aca126d8455ede5
ggaray <ggaray@mrhouston.net>
2025-02-22 21:51:44 +0100

    First

---
+++ app/home.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from gg_agents import travel_agent
+
+
+PASSWORD = st.secrets["log_in_pwd"]  # Change this to your desired password
+
+from huggingface_hub import InferenceClient
+import streamlit as st
+
+
+# client = InferenceClient("dslim/distilbert-NER",
+#                          token=st.secrets["all_token"])
+#
+#
+# aa = client.token_classification("Hello, my name is John Doe and I live in New York City.")
+#
+# print(aa)
+
+# Check if user is logged in
+if "authenticated" not in st.session_state:
+    st.session_state.authenticated = True
+
+def login():
+    if st.session_state["password"] == PASSWORD:
+        st.session_state.authenticated = True
+        st.success("Login successful!")
+    else:
+        st.error("Incorrect password. Try again.")
+
+def logout():
+    st.session_state.authenticated = False
+    st.success("Logged out successfully!")
+
+# Display login page if not authenticated
+if not st.session_state.authenticated:
+    st.title("🔒 Login")
+    st.text_input("Password", type="password", key="password")
+    st.button("Login", on_click=login)
+    st.stop()
+
+# If logged in, show the main content
+st.title("🏠 Welcome to the Streamlit Multipage App!")
+st.sidebar.button("Logout", on_click=logout)
+st.sidebar.success("Select a page above.")
+
+
+
